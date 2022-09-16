a433b13b498d5064938a26c1dc1ee21b1b667d89
Wirbainoua <wirbainouahadjaratou@github.com>
2022-09-16 12:06:27 -0700

    you got this

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void possitive_or_negative(int i);
+int lagest_number(int, int, int);
+void print_remainning_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
