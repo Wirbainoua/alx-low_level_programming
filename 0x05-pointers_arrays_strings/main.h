7a4f96e74600cc99a3fb6786a329ea1c8fad1149
Wirbainoua <wirbainouahadjaratou@github.com>
2022-09-19 09:43:17 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**
-* void prototypes()
-* int prototypes()
-*/
+ * void prototypes()
+ * int prototypes()
+ */
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -18,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
-#endif /*MAIN.H*/
+#endif /*MAIN_H*/
