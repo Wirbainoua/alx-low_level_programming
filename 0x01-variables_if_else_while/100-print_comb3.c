166d696f5856f9e0fa795f505a15a8b50f2b0174
Wirbainoua <wirbainouahadjaratou@github.com>
2022-09-21 12:15:18 -0700

    combination three

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 1;
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+
+	for (num2 = n; num2 <= 9 ; num2++)
+	{
+	putchar(num1 + '0');
+	putchar(num2 + '0');
+
+	if (num1 == 9 && num2 == 9)
+	continue;
+	else if (num1 == 8 && num2 == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+
+	}
+	n++;
+
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
