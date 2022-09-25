d850d0be3af853c1c1e2ec1c22a13f2f6107b4a7
Wirbainoua <wirbainouahadjaratou@github.com>
2022-09-25 12:30:09 -0700

    learn

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,16 +15,16 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[a] != '\0')
 	{
-	a++;
+        	a++;
 	}
 
 	b = 0;
 
 	while (src[b] != '\0')
 	{
-	dest[a] = src[b]; /*hello world */
-	a++;
-	b++;
+        	dest[a] = src[b]; /*hello world */
+        	a++;
+        	b++;
 	}
 
 	return (dest);
