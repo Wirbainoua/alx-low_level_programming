faf25e8d5de6686f5e67b2c5d0fc187b84beb805
Wirbainoua <wirbainouahadjaratou@github.com>
2022-09-26 01:38:46 -0700

    task one done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+
+	_putchar(letter);
+	}
+
+	_putchar('\n');
+
+}
